9cddf358787a0c24e60d54bf1101aa77fb5bd51b
Felipe Teixeira Schefer <felipe_official@outlook.com>
2022-01-09 12:06:44 -0300

    Created a Modal.js component that's used as a overlay to the Cart.js component by ReactDOM.createPortal()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Fragment} from "react";
+import Cart from "./components/Cart/Cart";
 import Header from './components/Layout/Header'
 import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
