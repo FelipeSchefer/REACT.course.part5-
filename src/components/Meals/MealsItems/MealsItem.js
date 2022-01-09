68d68a727992839aa64d49fe8c4f8ba4534719bd
Felipe Teixeira Schefer <felipe_official@outlook.com>
2022-01-08 22:48:42 -0300

    Added Meals and MealsItems folders and created componentes

---
+++ src/components/Meals/MealsItems/MealsItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './MealsItem.module.css'
+
+const MealsItem=(props) => {
+ const price = `$${props.price.toFixed(2)}`
+
+ return (
+  <li>
+   <div>
+    <h3> {props.name} </h3>
+    <div className={classes.description}> {props.description} </div>
+    <div className={classes.price}> {price} </div>
+   </div>
+   <div></div>
+  </li>
+ )
+}
+
+export default MealsItem
