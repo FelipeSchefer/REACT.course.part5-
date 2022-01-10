7a3c25a128c9d2329097b47a9abdb73a246ff38c
Felipe Teixeira Schefer <felipe_official@outlook.com>
2022-01-10 16:09:58 -0300

    Using the Contex; Adding a Cart Reducer; Working with useRef(); But the number in the Cart is showing undefined when it is add a food

--- src/components/Meals/MealsItems/MealsItem.js
+++ src/components/Meals/MealsItems/MealsItem.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import MealItemForm from './MealItemForm'
 import classes from './MealsItem.module.css'
+import CartContext from '../../../store/cart-context'
 
 const MealsItem=(props) => {
+ const cartCtx = useContext(CartContext)
  const price = `$${props.price.toFixed(2)}`
+
+ const addToCartHandler = amount =>{
+  cartCtx.addItem({
+   id: props.id,
+   name: props.name,
+   amount: props.amount,
+   price: props.price
+  })
+ }
 
  return (
   <li>
@@ -12,7 +23,9 @@ const MealsItem=(props) => {
     <div className={classes.description}> {props.description} </div>
     <div className={classes.price}> {price} </div>
    </div>
-   <div> <MealItemForm id={props.id}/> </div>
+   <div> 
+    <MealItemForm id={props.id} onAddToCart={addToCartHandler}/> 
+   </div>
   </li>
  )
 }
